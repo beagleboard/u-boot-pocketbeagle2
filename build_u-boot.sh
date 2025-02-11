fdb88374916554a8463dd98f6450e25034e658e3
Robert Nelson <robertcnelson@gmail.com>
2025-02-11 15:41:24 -0600

    ci: git bisect good bfaed6969c119673c3087ffd778b8e3e324c3202
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build_u-boot.sh
+++ build_u-boot.sh
@@ -75,6 +75,7 @@ git bisect bad 636fcc96c3d7e2b00c843e6da78ed3e9e3bdf4de
 git bisect good 1c2ffcd5e622031fa1c05335d0db839de14bf0e9
 git bisect good 8707ea0360046522d0784135b6c9a7c564f9515c
 git bisect good 5a287cf07aed6a60e25af903ea24bc0030d493b1
+git bisect good bfaed6969c119673c3087ffd778b8e3e324c3202
 echo "*************************************************"
 git bisect log
 echo "*************************************************"
