fea107f25529bbe24d3a888e2bac6a9413064301
Robert Nelson <robertcnelson@gmail.com>
2025-03-03 10:55:02 -0600

    Bump to: v2025.04-rc3-pocketbeagle2 and 11.00.05
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build_u-boot.sh
+++ build_u-boot.sh
@@ -12,11 +12,11 @@ ${CC64}gcc --version
 
 DIR=$PWD
 
-TI_FIRMWARE="11.00.02"
+TI_FIRMWARE="11.00.05"
 TRUSTED_FIRMWARE="v2.12.0"
 OPTEE="4.5.0"
 #UBOOT="v2025.01-pocketbeagle2"
-UBOOT="v2025.04-rc2-pocketbeagle2"
+UBOOT="v2025.04-rc3-pocketbeagle2"
 
 #rm -rf ./ti-linux-firmware/ || true
 if [ ! -d ./ti-linux-firmware/ ] ; then
