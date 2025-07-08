7d895ed91417f54ddef454580205cd7bb655fb5f
Robert Nelson <robertcnelson@gmail.com>
2025-07-08 18:45:40 -0500

    u-boot: v2025.07-am6232-pocketbeagle2, TI_FIRMWARE: 11.01.05, and OPTEE: 4.7.0-rc1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #https://github.com/beagleboard/ti-linux-firmware.git
-TI_FIRMWARE="${TI_FIRMWARE:-11.01.03}"
+TI_FIRMWARE="${TI_FIRMWARE:-11.01.05}"
 
 #https://github.com/TrustedFirmware-A/trusted-firmware-a.git
 TRUSTED_FIRMWARE="lts-v2.12.4"
 
 #https://github.com/OP-TEE/optee_os.git
-OPTEE="4.6.0"
+OPTEE="4.7.0-rc1"
 
 #https://github.com/beagleboard/u-boot.git
-UBOOT="${UBOOT:-v2025.07-rc5-am6232-pocketbeagle2}"
+UBOOT="${UBOOT:-v2025.07-am6232-pocketbeagle2}"
