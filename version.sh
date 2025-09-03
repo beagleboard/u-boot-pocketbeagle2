d6c9d3819840be6d91b70531aa9d16c65ac63af4
Robert Nelson <robertcnelson@gmail.com>
2025-09-03 11:56:04 -0500

    bump: TI_FIRMWARE:11.01.12; TRUSTED_FIRMWARE:lts-v2.12.6; OPTEE:4.7.0
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #https://github.com/beagleboard/ti-linux-firmware.git
-TI_FIRMWARE="${TI_FIRMWARE:-11.01.05}"
+TI_FIRMWARE="${TI_FIRMWARE:-11.01.12}"
 
 #https://github.com/TrustedFirmware-A/trusted-firmware-a.git
-TRUSTED_FIRMWARE="lts-v2.12.4"
+TRUSTED_FIRMWARE="lts-v2.12.6"
 
 #https://github.com/OP-TEE/optee_os.git
-OPTEE="4.7.0-rc1"
+OPTEE="4.7.0"
 
 #https://github.com/beagleboard/u-boot.git
 UBOOT="${UBOOT:-v2025.07-am6232-pocketbeagle2}"
