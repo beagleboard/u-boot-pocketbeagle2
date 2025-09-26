33b09f0b62a7bb8a91f516c22d53efefc7aafa34
Robert Nelson <robertcnelson@gmail.com>
2025-09-26 12:33:02 -0500

    ci: back to production pb2 512MB
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -10,4 +10,5 @@ TRUSTED_FIRMWARE="lts-v2.12.6"
 OPTEE="4.7.0"
 
 #https://github.com/beagleboard/u-boot.git
-UBOOT="${UBOOT:-v2025.10-rc5-am62-pocketbeagle2-1GB}"
+UBOOT="${UBOOT:-v2025.10-rc5-am62-pocketbeagle2}"
+#UBOOT="${UBOOT:-v2025.10-rc5-am62-pocketbeagle2-1GB}"
