91dd43611c70f59f8456c7f9f9cd8f5c1c8f6316
Robert Nelson <robertcnelson@gmail.com>
2025-11-11 12:14:38 -0600

    ci: wire in rebuild offset
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/push-tag.sh
+++ scripts/push-tag.sh
@@ -5,16 +5,16 @@ wfile=$(mktemp /tmp/builder.XXXXXXXXX)
 . version.sh
 
 echo "****************************************************"
-echo [${UBOOT}:${TRUSTED_FIRMWARE}:${OPTEE}:${TI_FIRMWARE}]
+echo [${UBOOT}:${TFA}:${OPTEE}:${TI_FIRMWARE}:${REBUILD}]
 echo "****************************************************"
 
 cp public/base.sh public/get_n_install.sh
-sed -i -e 's:TAG:'${UBOOT}'-'${TI_FIRMWARE}':g' public/get_n_install.sh
+sed -i -e 's:TAG:'${UBOOT}'-'${TI_FIRMWARE}'-'${REBUILD}':g' public/get_n_install.sh
 
-echo "${UBOOT}-${TI_FIRMWARE} release" > ${wfile}
+echo "${UBOOT}-${TI_FIRMWARE}-${REBUILD} release" > ${wfile}
 
 git commit -a -F ${wfile} -s
 
-git tag -a ${UBOOT}-${TI_FIRMWARE} -m "${UBOOT}-${TI_FIRMWARE}"
+git tag -a ${UBOOT}-${TI_FIRMWARE}-${REBUILD} -m "${UBOOT}-${TI_FIRMWARE}-${REBUILD}"
 
 git push origin main --tags
