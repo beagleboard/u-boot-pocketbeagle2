4980a8db260fc27311e00b32c65c70b3ebd29fae
Robert Nelson <robertcnelson@gmail.com>
2026-01-15 17:10:11 -0600

    bump: TFA:-lts-v2.12.8
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -5,7 +5,8 @@ TI_FIRMWARE="${TI_FIRMWARE:-11.02.11}"
 
 #https://github.com/TrustedFirmware-A/trusted-firmware-a.git
 TFA_GIT="${TFA_GIT:-https://github.com/TrustedFirmware-A/trusted-firmware-a.git}"
-TFA="${TFA:-lts-v2.12.8}"
+#TFA="${TFA:-lts-v2.12.8}"
+TFA="${TFA:-v2.14.0}"
 
 #https://github.com/OP-TEE/optee_os.git
 OPTEE="${OPTEE:-4.9.0-rc1}"
