b7372c02f30c97420d532049331c605bbc007d9e
Robert Nelson <robertcnelson@gmail.com>
2026-01-15 16:36:56 -0600

    bump: TI_FIRMWARE:-11.02.11, TFA:-lts-v2.12.8, OPTEE:-4.9.0-rc1, UBOOT:-v2025.10-am62-pocketbeagle2
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 #https://github.com/TexasInstruments/ti-linux-firmware.git
-TI_FIRMWARE="${TI_FIRMWARE:-11.02.04}"
+TI_FIRMWARE="${TI_FIRMWARE:-11.02.11}"
 
 #https://github.com/TrustedFirmware-A/trusted-firmware-a.git
 TFA_GIT="${TFA_GIT:-https://github.com/TrustedFirmware-A/trusted-firmware-a.git}"
-TFA="lts-v2.12.8"
+TFA="${TFA:-lts-v2.12.8}"
 
 #https://github.com/OP-TEE/optee_os.git
-OPTEE="4.8.0"
+OPTEE="${OPTEE:-4.9.0-rc1}"
 
 #https://github.com/beagleboard/u-boot.git
 UBOOT_GIT="${UBOOT_GIT:-https://github.com/beagleboard/u-boot.git}"
-UBOOT="${UBOOT:-v2026.01-rc2-am62-pocketbeagle2}"
+UBOOT="${UBOOT:-v2025.10-am62-pocketbeagle2}"
 #UBOOT="${UBOOT:-v2025.10-am62-pocketbeagle2-1GB}"
