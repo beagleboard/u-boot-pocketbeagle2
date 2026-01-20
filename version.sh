8b96b1a9a1fb15831e6152d1d58a9c1dc0259de4
Robert Nelson <robertcnelson@gmail.com>
2026-01-20 11:06:27 -0600

    bump: OPTEE:-4.9.0
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -8,7 +8,7 @@ TFA_GIT="${TFA_GIT:-https://github.com/TrustedFirmware-A/trusted-firmware-a.git}
 TFA="${TFA:-v2.14.0}"
 
 #https://github.com/OP-TEE/optee_os.git
-OPTEE="${OPTEE:-4.9.0-rc1}"
+OPTEE="${OPTEE:-4.9.0}"
 
 #https://github.com/beagleboard/u-boot.git
 UBOOT_GIT="${UBOOT_GIT:-https://github.com/beagleboard/u-boot.git}"
