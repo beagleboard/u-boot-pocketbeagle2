0f837dcb092560db47cdab3afaa833e4b72705d3
Robert Nelson <robertcnelson@gmail.com>
2026-01-30 13:51:44 -0600

    v2026.01-am62-pocketbeagle2-11.02.11 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- public/get_n_install.sh
+++ public/get_n_install.sh
@@ -17,9 +17,9 @@ if [ -f ./u-boot.img ] ; then
 	rm -rf ./u-boot.img || true
 fi
 
-wget https://github.com/beagleboard/u-boot-pocketbeagle2/releases/download/v2025.10-am62-pocketbeagle2-11.02.04/tiboot3.bin
-wget https://github.com/beagleboard/u-boot-pocketbeagle2/releases/download/v2025.10-am62-pocketbeagle2-11.02.04/tispl.bin
-wget https://github.com/beagleboard/u-boot-pocketbeagle2/releases/download/v2025.10-am62-pocketbeagle2-11.02.04/u-boot.img
+wget https://github.com/beagleboard/u-boot-pocketbeagle2/releases/download/v2026.01-am62-pocketbeagle2-11.02.11/tiboot3.bin
+wget https://github.com/beagleboard/u-boot-pocketbeagle2/releases/download/v2026.01-am62-pocketbeagle2-11.02.11/tispl.bin
+wget https://github.com/beagleboard/u-boot-pocketbeagle2/releases/download/v2026.01-am62-pocketbeagle2-11.02.11/u-boot.img
 
 sync
 
